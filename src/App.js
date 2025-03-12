1c29b77d2d4fd6a0e0077166e189c5b39ceba077
Wilson-Wu1 <wilsonwuuu@gmail.com>
2025-03-12 13:58:33 -0700

    feat: Add google analytics

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import Navbar from './navbar';
 import HeaderInfo from './HeaderInfo';
 import Bottom from './bottom';
 
+import { useEffect } from "react";
 
+const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID
+
+const useGoogleAnalytics = () => {
+  useEffect(() => {
+    // Inject the Google Analytics script dynamically
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+    document.head.appendChild(script);
+
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag("js", new Date());
+    gtag("config", GA_TRACKING_ID);
+  }, []);
+};
 
 function App() {
+  useGoogleAnalytics();
   return (
     <div className="App">
       <Navbar/>
