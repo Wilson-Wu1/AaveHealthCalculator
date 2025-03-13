7f1f8f475cd90ed3988022e5ab1e994b3e1a6804
Wilson <wilsonwuuu@gmail.com>
2025-03-12 19:06:51 -0700

    test

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import Bottom from './bottom';
 
 import { useEffect } from "react";
 
-const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID
+// const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID
 
-const useGoogleAnalytics = () => {
-  useEffect(() => {
-    // Inject the Google Analytics script dynamically
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
-    document.head.appendChild(script);
+// const useGoogleAnalytics = () => {
+//   useEffect(() => {
+//     // Inject the Google Analytics script dynamically
+//     const script = document.createElement("script");
+//     script.async = true;
+//     script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+//     document.head.appendChild(script);
 
-    // Initialize Google Analytics
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag("js", new Date());
-    gtag("config", GA_TRACKING_ID);
-  }, []);
-};
+//     // Initialize Google Analytics
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag(){window.dataLayer.push(arguments);}
+//     gtag("js", new Date());
+//     gtag("config", GA_TRACKING_ID);
+//   }, []);
+// };
 
 function App() {
-  useGoogleAnalytics();
+  // useGoogleAnalytics();
   return (
     <div className="App">
       <Navbar/>
